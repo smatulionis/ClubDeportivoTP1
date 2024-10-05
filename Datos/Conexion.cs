901824b05164a7a73b8d8c3c1779aa35863f8b42
Sebastian <smatulionis@live.com.ar>
2024-10-05 12:19:58 -0300

    Update Conexion.cs

--- Datos/Conexion.cs
+++ Datos/Conexion.cs
@@ -22,7 +22,7 @@ namespace ClubDeportivo.Datos
             this.servidor = "localhost";
             this.puerto = "3306";
             this.usuario = "root";
-            this.clave = "";
+            this.clave = "atila";
         }
         public MySqlConnection CrearConexion()
         {
